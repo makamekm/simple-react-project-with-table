0b08f1d0ed0f5c754b3150113e19a76fd8ae7f15
makame <makame@mail.ru>
2020-11-02 22:06:07 +0100

    fix: add react-service-provider demo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { observer, enableStaticRendering } from "mobx-react";
+import React from "react";
+import { useServiceProvider } from "react-service-provider";
+import { Services } from "~/components/Services";
+
+if (typeof window === "undefined") {
+  enableStaticRendering(true);
+}
+
+const App = observer(({ Component, pageProps }) => {
+  const [ServiceProvider, ServiceProviderHook] = useServiceProvider(
+    ...Services
+  );
+  return (
+    <ServiceProvider>
+      <ServiceProviderHook>
+        <Component {...pageProps} />
+      </ServiceProviderHook>
+    </ServiceProvider>
+  );
+});
+
+export default App;
